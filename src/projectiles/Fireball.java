1e5fa1699aaa35f26e93ae382ce43efc0abfdf5a
Peiman Timaji <Peiman.Timaji@city.ac.uk>
2024-03-10 13:09:35 +0000

    Final commit before submitting

--- src/projectiles/Fireball.java
+++ src/projectiles/Fireball.java
@@ -7,7 +7,7 @@ public class Fireball extends DynamicBody {
 
   public Fireball(GameWorld world, Shape shape) {
     super(world, shape);
-    //TODO Auto-generated constructor stub
+    
   }
   
 }
